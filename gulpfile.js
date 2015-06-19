0610ecf24b700b9e59563d90970950bdd74d4d94
Michael Brown <mike.brown@allianz.com.au>
2015-06-19 10:48:01 +1000

    Setting up gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var vendors = [
+  "bootstrap", "bootstrap-slider", "detect-browser", "jquery", "react"
+];
+
+var PATH = {
+   "src": "src",
+   "srcjs": "src/js",
+   "distsrc": "dist/src",
+   "distsrcjs": "dist/src/js",
+};
+
+
+// gulp.task('vendor', function() {
+//    // A dummy entry point for browserify
+//    var stream = gulp.src('./gulp/noop.js', {
+//          read: false
+//       })
+//       .pipe(browserify({
+//          debug: false, // Don't provide source maps for vendor libs
+//       }))
+//       .on('prebundle', function(bundle) {
+//          // Require vendor libraries and make them available outside the bundle.
+//          libs.forEach(function(lib) {
+//             bundle.require(lib);
+//          });
+//       });
+
+//    // Give the destination file a name, adding '.min' if this is production
+//    stream.pipe(rename('vendor' + (production ? '.min' : '') + '.js'))
+
+//    // Save to the build directory
+//    .pipe(gulp.dest('build/'));
+
+//    return stream;
+
+// });
+
+gulp.task('build', function() {
+   browserify({
+         entries: PATH.srcjs + '/index.jsx',
+         extensions: ['.jsx'],
+         debug: true
+      })
+      .transform(babelify)
+      .pipe(source())
+      .pipe(gulp.dest(distsrc));
+});
+
+gulp.task('buildprod', function() {
+   browserify({
+         entries: PATH.srcjs + '/index.jsx',
+         extensions: ['.jsx'],
+         debug: true
+      })
+      .transform(babelify)
+      .bundle()
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('dist'));
+});
+
+
+
+// vendors and app tasks from http://www.kellyjandrews.com/2015/04/01/modular-react-components-with-browserify.html
+gulp.task('vendors', function () {
+    var stream = browserify({
+            debug: false,
+            require: vendors
+        });
+
+    stream.bundle()
+          .pipe(source('vendors.js'))
+          .pipe(gulp.dest(PATH.distsrcjs));
+    return stream;
+});
+
+
+gulp.task('app', function () {
+    var stream = browserify({
+            entries: [PATH.srcjs + '/index.jsx'],
+            transform: [babelify],
+            debug: false,
+            extensions: ['.jsx'],
+            fullPaths: false
+        });
+
+    vendors.forEach(function(vendor) {
+        stream.external(vendor);
+    });
+
+    return stream.bundle()
+                 .pipe(source('index.js'))
+                 .pipe(gulp.dest(PATH.distsrcjs));
+});
+
+
+
+gulp.task('default', ['build']);
---
+++ src/js/SliderNativeBootstrap.jsx
@@ -0,0 +1,81 @@
+var BoostrapSlider = require("BoostrapSlider.jsx");
+var SliderNative = require("./SliderNative.jsx");
+
+var BrowserDetectMixin = {
+    // This needs to go elsewhere, e.g. load from another component.
+    // Lifted from:
+    // http://stackoverflow.com/questions/19999388/jquery-check-if-user-is-using-ie/21712356#21712356
+    detectIE: function () {
+        var ua = window.navigator.userAgent;
+
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11 => return version number
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+
+        var edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+           // IE 12 => return version number
+           return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+
+        // other browser
+        return false;
+    }
+};
+
+
+
+module.exports = React.createClass({
+    // SliderNativeBootstrap
+    componentWillMount: function () {
+        // Although IE10+ displays the native range control,it:
+        //      a) looks crap
+        //      b) doesn't respond to its Input or Change events properly.
+        // So have augmented a feature test with some good, old-fashioned
+        // browser sniffing to always display the Bootstrap version on IE.
+        var ieVersion = BrowserDetectMixin.detectIE();
+        if (ieVersion > 1 && ieVersion < 12) {
+                // IE up to version 11
+                this.supportsRange = false;
+        }
+        else {
+            // IE 12+ and all other browsers
+            // Test whether range input is accepted by creating such a field, then seeing what its
+            // type is set to.
+            var input = document.createElement('input');
+            input.setAttribute('type', 'range');
+            this.supportsRange = input.type !== "text" ? true : false;        
+        }
+
+        
+    },
+    render: function () {
+        var polyfill = typeof this.props.polyfill == "undefined" ? true : this.props.polyfill;
+        if(polyfill) {
+            if(this.supportsRange) {
+                return (
+                    <SliderNative {...this.props} />
+                );
+            }
+            else {
+                return (
+                    <BootstrapSlider {...this.props} />
+                );
+            }
+        }
+        else {
+            return (
+                <BootstrapSlider {...this.props} />
+            );            
+        }
+    }
+});
